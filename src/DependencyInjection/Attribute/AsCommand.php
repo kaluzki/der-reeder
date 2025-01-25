11f18e01520f1d017049535dacfe89a770d86136
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2025-01-25 21:45:08 +0100

    Add `process` option to `AsCommand` attribute
    #[AsCommand('foo:bar', process: ['alternate_screen' => true])]

--- src/DependencyInjection/Attribute/AsCommand.php
+++ src/DependencyInjection/Attribute/AsCommand.php
@@ -5,9 +5,15 @@ namespace Kaluzki\DerReeder\DependencyInjection\Attribute;
 use Kaluzki\DerReeder\DependencyInjection\ValueResolver;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Cursor;
 use Symfony\Component\Console\Helper\HelperSet;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\Input;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\StreamableInputInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\OutputStyle;
 use Symfony\Component\Console\Style\StyleInterface;
@@ -19,10 +25,21 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadataFactory;
+use Symfony\Component\Process\Process;
 
 #[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD)]
 class AsCommand extends \Symfony\Component\Console\Attribute\AsCommand
 {
+    public function __construct(
+        public string $name,
+        public ?string $description = null,
+        array $aliases = [],
+        bool $hidden = false,
+        public readonly ?array $process = null
+    ) {
+        parent::__construct($name, $this->description, $aliases, $hidden);
+    }
+
     public static function passHandler(
         self $attr,
         ContainerBuilder $container,
@@ -40,6 +57,7 @@ class AsCommand extends \Symfony\Component\Console\Attribute\AsCommand
                 new Definition(arguments: [
                     [new Reference($id), $reflector->getName()],
                     new Reference($commandId),
+                    $attr->process,
                 ])->setFactory([static::class, 'createCode'])
             ]);
 
@@ -57,6 +75,54 @@ class AsCommand extends \Symfony\Component\Console\Attribute\AsCommand
         }
     }
 
+    public static function createCode(callable $code, Command $command, ?array $process = null): callable
+    {
+        /** @var ?Application $app */
+        ($app = $command->getApplication()) instanceof Application || $app = null;
+        if ($process && !getenv($varName = md5(__METHOD__))) {
+            return self::subProcess($_SERVER['argv'] ?? [], $process, $varName);
+        }
+        return function (InputInterface $input, OutputInterface $output) use ($code, $app) {
+            $resolver = self::resolver($input, $output, $app);
+            $args = $resolver->getArguments(new Request(), $code);
+            $return = $code(...$args);
+            foreach (is_iterable($return) ? $return : [] as $line) {
+                $output->writeln($line);
+            }
+            return is_int($return) ? $return : Command::SUCCESS;
+        };
+    }
+
+    private static function subProcess(array $command, array $config, string $varName): callable
+    {
+        return function () use ($varName, $command, $config) {
+            $config += [
+                'tty' => true,
+                'pty' => true,
+                'options' => ['create_new_console' => true],
+                'restore_stty' => true,
+                'alternate_screen' => false, // https://unix.stackexchange.com/questions/27941/
+            ];
+
+            $process = new Process($command, env: [$varName => true]);
+            $process->setOptions($config['options']);
+            $process->setTty($config['tty']);
+            $process->setPty($config['pty']);
+            try {
+                $sttyMode = shell_exec('stty -g');
+                if ($config['alternate_screen']) {
+                    echo shell_exec('tput smcup'); // echo "\e[7\e[?47h";
+                }
+                return $process->run();
+            } finally {
+                if ($config['alternate_screen']) {
+                    echo shell_exec('tput rmcup'); // echo "\e[2J\e[?47l\e8";
+                }
+                $config['restore_stty'] && shell_exec(\sprintf('stty %s', $sttyMode));
+            }
+        };
+    }
+
     /**
      * @return ArgumentMetadata[]
      */
@@ -68,43 +134,43 @@ class AsCommand extends \Symfony\Component\Console\Attribute\AsCommand
         );
     }
 
-    public static function createCode(callable $code, Command $command): callable
+    private static function resolver(InputInterface $input, OutputInterface $output, ?Application $app = null): ArgumentResolver
     {
-        return function (InputInterface $input, OutputInterface $output) use ($code, $command) {
-            $app = $command->getApplication();
-            $container = $app instanceof Application ? $app->getKernel()->getContainer() : null;
-            $resolver = new ArgumentResolver(
-                argumentValueResolvers: [
-                    new ValueResolver(function (ArgumentMetadata $arg) use ($container, $input, $output, $app) {
-                        if (!strpos($type = $arg->getType(), '\\')) {
-                            return;
-                        }
-                        yield from match ($type) {
-                            InputInterface::class => [$input],
-                            OutputInterface::class => [$output],
-                            OutputStyle::class, SymfonyStyle::class, StyleInterface::class => [new SymfonyStyle($input, $output)],
-                            HelperSet::class => [$app->getHelperSet()],
-                            default => $container?->has($type) ? [$container?->get($type)] : [],
-                        };
-                    }),
-                    new ValueResolver(function (ArgumentMetadata $arg) use ($input) {
-                        $name = $arg->getName();
-                        if ($input->hasArgument($name)) {
-                            yield $input->getArgument($name);
-                        } else if ($input->hasOption($name)) {
-                            yield $input->hasOption($name);
-                        }
-                    }),
-                    ...ArgumentResolver::getDefaultArgumentValueResolvers(),
-                ]
-            );
-            $args = $resolver->getArguments(new Request(), $code);
-            $return = $code(...$args);
-            foreach (is_iterable($return) ? $return : [] as $line) {
-                $output->writeln($line);
-            }
-            return is_int($return) ? $return : Command::SUCCESS;
-        };
+        $container = $app?->getKernel()?->getContainer();
+        return new ArgumentResolver(
+            argumentValueResolvers: [
+                new ValueResolver(function (ArgumentMetadata $arg) use ($input, $output, $app, $container) {
+                    if (!strpos($type = $arg->getType(), '\\')) {
+                        return;
+                    }
+                    yield from match ($type) {
+                        Input::class,
+                        ArgvInput::class,
+                        InputInterface::class => $input instanceof $type ? [$input] : [],
+                        OutputInterface::class,
+                        ConsoleOutputInterface::class,
+                        ConsoleOutput::class => $output instanceof $type ? [$output] : [],
+                        OutputStyle::class,
+                        SymfonyStyle::class,
+                        StyleInterface::class => [new SymfonyStyle($input, $output)],
+                        Cursor::class => [new Cursor(
+                            $output,
+                            $input instanceof StreamableInputInterface ? $input->getStream() : null
+                        )],
+                        HelperSet::class => $app ? [$app->getHelperSet()] : [],
+                        default => $container?->has($type) ? [$container->get($type)] : [],
+                    };
+                }),
+                new ValueResolver(function (ArgumentMetadata $arg) use ($input) {
+                    $name = $arg->getName();
+                    if ($input->hasArgument($name)) {
+                        yield $input->getArgument($name);
+                    } else if ($input->hasOption($name)) {
+                        yield $input->hasOption($name);
+                    }
+                }),
+                ...ArgumentResolver::getDefaultArgumentValueResolvers(),
+            ]
+        );
     }
-
 }
